8aafdccd2039ca81ad57cec1686d699b68184852
harvidston <harvidston@mail.ru>
2025-07-28 00:04:26 -0700

    feat(wezterm): upgrade wezterm config for macos

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,9 +2,12 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+config.window_decorations = "RESIZE"
 
-config.window_decorations = "NONE"
+config.initial_cols = 320
+config.initial_rows = 100
 
+--config.native_macos_fullscreen_mode = true
 --wezterm.on('gui-startup', function(cmd)
 --  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
 --  window:gui_window():toggle_fullscreen()
@@ -12,9 +15,10 @@ config.window_decorations = "NONE"
 
 config.font = wezterm.font {
 	family = 'JetBrains Mono',
-    weight = 'Regular',
-}
-config.font_size = 13.5
+	weight = 'Regular',
+ }
+
+config.font_size = 13.0
 config.color_scheme = "kanagawabones"
 --config.color_scheme = 'Black Metal (Bathory) (base16)'
 --config.color_scheme = 'Twilight (Gogh)'
@@ -23,9 +27,10 @@ config.color_scheme = "kanagawabones"
 --config.color_scheme = 'ForestBlue'
 --config.color_scheme = 'Darktooth (base16)'
 config.window_frame = {
-  active_titlebar_bg = "#0F0F12",
-  inactive_titlebar_bg = "#0F0F12",
-  font_size = 10.0,
+  --active_titlebar_bg = "#0F0F12",
+  active_titlebar_bg = "#15151A",
+  inactive_titlebar_bg = "#15151A",
+  font_size = 11.0,
 }
 config.colors = {
   background = "#15151A"
@@ -38,12 +43,12 @@ config.inactive_pane_hsb = {
 config.keys = {
   {
     key = 'v',
-    mods = 'CTRL|ALT',
+    mods = 'SHIFT|CTRL',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   {
     key = 'h',
-    mods = 'CTRL|ALT',
+    mods = 'SHIFT|CTRL',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
